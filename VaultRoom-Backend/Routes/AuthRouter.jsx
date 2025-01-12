c77c0fa0706238ec28dace369355bb562c57326e
Gaurav Kumar <145714609+Gaurav-k-Gupta@users.noreply.github.com>
2025-01-12 20:32:11 +0530

    Delete VaultRoom-Backend directory

--- VaultRoom-Backend/Routes/AuthRouter.jsx
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const Router = express.Router();
-const { googleAuth } = require('../Controllers/AuthControllers.jsx');
-
-Router.get("/google", googleAuth);
-
-module.exports = Router;
